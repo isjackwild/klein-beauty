1c5d94b03d0222af020d4a80ff9d0c6b50654c67
Jack Wild <jack.wild@live.co.uk>
2018-01-23 13:07:07 +0000

    added loader assets as base 64

--- app/components/Loader.js
+++ app/components/Loader.js
@@ -1,13 +1,17 @@
 // NPM
 import { h } from 'preact';
 import { connect } from 'preact-redux';
+import { KLEIN_WORDMARK_B64, LOADER_SURROUND_b64 } from '../CONSTANTS';
 
 const View = ({ isVisible, loadProgress }) => {
 	return (
 		<div class={`loader loader--${isVisible ? 'visible' : 'hidden'}`}>
 			<div class={`loader__inner loader__inner--${isVisible ? 'visible' : 'hidden'}`}>
-				<img class="loader__image" src="assets/images/ui/loading-klein.jpg" />
-				<img class="loader__progress" src="assets/images/ui/loading-bar.jpg" />
+				<img class="loader__image" src={`data:image/png;base64, ${KLEIN_WORDMARK_B64}`} />
+				<div class="loader__progress">
+					<div class="loader__progress-bar" style={{ width: `${loadProgress * 100}%` }}></div>
+					<img class="loader__progress-surround" src={`data:image/png;base64, ${LOADER_SURROUND_b64}`} />
+				</div>
 			</div>
 		</div>
 	);
